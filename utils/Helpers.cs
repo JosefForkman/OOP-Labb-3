679b1a077e73f9947b5219fac50def4867bcd88b
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2024-11-08 13:09:05 +0100

    Create Helpers.cs

---
+++ utils/Helpers.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Helpers
+{
+    public static string Ask(string question)
+    {
+        Console.WriteLine(question);
+        return Console.ReadLine() ?? "";
+    }
+}
